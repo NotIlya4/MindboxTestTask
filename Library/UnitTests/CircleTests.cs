69279e803f0f664a314e2c03eb4116c7329b2e7c
NotIlya <secretman44444@gmail.com>
2023-06-05 19:59:49 +0300

    Add tests

---
+++ Library/UnitTests/CircleTests.cs
@@ -0,0 +1,27 @@
+﻿using MindboxTestTaskLibrary;
+
+namespace UnitTests;
+
+public class CircleTests
+{
+    [Theory]
+    [InlineData(78.54, 5)]
+    [InlineData(314.16, 10)]
+    [InlineData(19.63, 2.5)]
+    public void Area_ValidRadius_CalculateArea(double expect, double radius)
+    {
+        var circle = new Circle(radius);
+
+        double result = circle.Area;
+        
+        Assert.Equal(expect, result, 0.01);
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void Constructor_InvalidRadius_Throw(double radius)
+    {
+        Assert.Throws<ArgumentException>(() => new Circle(radius));
+    }
+}
